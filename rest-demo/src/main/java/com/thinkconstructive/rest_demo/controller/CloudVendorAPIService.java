52dc28737336a9ccb89735cbb0f31b6f5a18f539
harshad.kamble <harshadkamble30@gmail.com>
2026-04-23 14:49:41 +0530

    2nd commit.

--- rest-demo/src/main/java/com/thinkconstructive/rest_demo/controller/CloudVendorAPIService.java
+++ rest-demo/src/main/java/com/thinkconstructive/rest_demo/controller/CloudVendorAPIService.java
@@ -1,7 +1,11 @@
 package com.thinkconstructive.rest_demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +15,35 @@ import com.thinkconstructive.rest_demo.model.CloudVendor;
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
 	
+	CloudVendor cloudVendor;
+	
 	@GetMapping("/{vendorId}")
 	public CloudVendor getCloudVendorDetails(@PathVariable("vendorId") String vendorId) {
 		
-		return new CloudVendor("C1", "vendor 1", "Address One", "xxxxx");
+		return cloudVendor;
+		
+		//return new CloudVendor("C1", "vendor 1", "Address One", "xxxxx");
+	}
+	
+	@PostMapping
+	public String createCloudVendorDetails( @RequestBody CloudVendor cloudVendor) {
+		
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Created Successfully.";
+	}
+	
+	@PutMapping
+	public String updateCloudVendorDetails( @RequestBody CloudVendor cloudVendor) {
+		
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Updated Successfully.";
+	}
+	
+	@DeleteMapping("/{vendorId}")
+	public String deleteCloudVendorDetails(@PathVariable("vendorId") String vendorId) {
+		
+		this.cloudVendor = null;
+		return "Cloud Vendor Deleted Successfully.";
 	}
 
 }
